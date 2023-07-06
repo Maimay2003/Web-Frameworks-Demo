02d377dcb3f659789b6054588a1a36d27fd36534
Maymouna Bah <mb33@williams.edu>
2023-07-06 18:18:36 +0000

    More Changes Made

--- demo.py
+++ demo.py
@@ -28,7 +28,7 @@ def register():
 @app.route("/update_server", methods=['POST'])
 def webhook():
     if request.method == 'POST':
-        repo = git.Repo('/home/maimay2003/Web-Frameworks-Demo')
+        repo = git.Repo('/home/Maimay2003/Web-Frameworks-Demo')
         origin = repo.remotes.origin
         origin.pull()
         return 'Updated PythonAnywhere successfully', 200
